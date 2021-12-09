d4c094ae19a036950e18890b467640c71bb805bc
bartvriesema <bart@vriesema.org>
2021-12-09 15:53:00 +0100

    Header and buttons added. Did some simple styling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      Begin hier met de opdracht!
-    </div>
-  );
+    return (
+        <div>
+            <h1>Pokemon app</h1>
+            <div className="pokemon-button-wrapper">
+                <button className="pokemon-button">Previous</button>
+                <button className="pokemon-button">Next</button>
+            </div>
+
+
+        </div>
+    );
 }
 
 export default App;
